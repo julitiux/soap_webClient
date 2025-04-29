3716303aee57004ef1ec6d7401606917bb1925d9
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-04-29 00:39:28 -0600

    starting new project soap_webClient

---
+++ src/main/java/com/soap_webClient/SoapWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.soap_webClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soap_webClient/SoapWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap_webClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
